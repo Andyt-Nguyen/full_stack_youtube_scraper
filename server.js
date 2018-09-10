1582229f4634d31c91d78dc130deefc85edf7d46
Andrew Ty Nguyen <andyt.nguyen123@gmail.com>
2018-09-10 11:32:29 -0700

    get route file

--- server.js
+++ server.js
@@ -29,7 +29,7 @@ app.use(express.static(path.join(__dirname,'media/build')))
 
 
 app.get('*', (req,res) =>{
-    res.sendFile(path.join(__dirname+'/media/build/index.html'));
+    res.sendFile(path.join(__dirname,'media/build/index.html'));
 });
 
 
